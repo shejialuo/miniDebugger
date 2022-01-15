f44b60e0d8d8b060a5fbdf761d313546b6a92d37
shejialuo <shejialuo@gmail.com>
2022-01-15 18:55:12 +0800

    Add source code display support
    Linux use ELF and DWARF to store the specific information. This commit
    use `libelfin` to parse the information for convenience.
    
    This commit also changes the signal handling for elegance. When
    receiving siganl, choose the specific siganl to handle, and wrap the PC
    back operation. So the function `stepOverBreakpoint` can delete the PC
    operation, and uses the function `waitForSignal` to solve this problem
    elegantly.
    
    Also, in the `handleSiganlTrap` function, this commit use `printSource`
    function to print the current line of the program text.

--- include/debugger.h
+++ include/debugger.h
@@ -6,6 +6,9 @@
 #include <unordered_map>
 #include <cstdint>
 #include <unistd.h>
+#include "signal.h"
+#include "elf/elf++.hh"
+#include "dwarf/dwarf++.hh"
 #include "mem.h"
 #include "breakpoint.h"
 #include "reg.h"
@@ -16,8 +19,14 @@ private:
   std::string programName;
   // process id
   pid_t pid;
+  // load address
+  uint64_t loadAddress = 0;
   // breakpoint
   std::unordered_map<std::intptr_t, Breakpoint> breakpoints;
+  // dwarf
+  dwarf::dwarf pDwarf;
+  // elf
+  elf::elf pElf;
   // memory class
   Memory memory;
   // To handle user input
@@ -38,6 +47,20 @@ private:
   void waitForSignal();
   // Step over the breakpoint, note that stepping over.
   void stepOverBreakpoint();
+  // Get function from PC
+  dwarf::die getFunctionFromPC(uint64_t pc);
+  // Get line entry from PC
+  dwarf::line_table::iterator getLineEntryFromPC(uint64_t pc);
+  // To get the mapped load address
+  void initializeLoadAddress();
+  // To calculate the offset from the load address
+  uint64_t offsetLoadAddress(uint64_t address);
+  // Print the source code
+  void printSource(const std::string& fileName,unsigned line, unsigned nLinesContext = 2);
+  // Get the signal information
+  siginfo_t getSignalInfo();
+  // Handle the signal
+  void handleSignaltrap(siginfo_t info);
 public:
   Debugger(std::string name, pid_t p);
   // Run the loop
--- src/debugger.cpp
+++ src/debugger.cpp
@@ -1,18 +1,29 @@
 #include <vector>
 #include <sstream>
+#include <fstream>
 #include <algorithm>
 #include <stdexcept>
+#include <fcntl.h>
+#include "signal.h"
 #include "sys/wait.h"
 #include "sys/ptrace.h"
 #include "sys/user.h"
 #include "linenoise.h"
 #include "spdlog/spdlog.h"
+#include "elf/elf++.hh"
+#include "dwarf/dwarf++.hh"
 #include "breakpoint.h"
 #include "reg.h"
 #include "debugger.h"
 
-Debugger::Debugger(std::string name, pid_t p)
-    : programName{name}, pid{p}, memory{p} {}
+Debugger::Debugger(std::string name, pid_t p): memory(p) {
+  programName = name;
+  pid = p;
+  auto fd = open(programName.c_str(), O_RDONLY);
+  pElf = elf::elf{elf::create_mmap_loader(fd)};
+  pDwarf = dwarf::dwarf{dwarf::elf::create_loader(pElf)};
+  close(fd);
+}
 
 std::vector<std::string> Debugger::split(const std::string &s, char delimiter) {
   std::vector<std::string> out{};
@@ -34,9 +45,7 @@ bool Debugger::isPrefix(const std::string& s, const std::string& of) {
 void Debugger::continueExecution() {
   stepOverBreakpoint();
   ptrace(PTRACE_CONT, pid, nullptr, nullptr);
-  int waitStatus;
-  int options = 0;
-  waitpid(pid, &waitStatus, options);
+  waitForSignal();
 }
 
 void Debugger::setBreakPointAtAddress(std::intptr_t addr) {
@@ -47,22 +56,125 @@ void Debugger::setBreakPointAtAddress(std::intptr_t addr) {
 }
 
 void Debugger::stepOverBreakpoint() {
-  // Remember to substract 1, the the CPU
-  // execute INT3, the PC will add 1 and traps.
-  uint64_t maybeBreakPointLocation = memory.getPC() - 1;
 
   // The address must be stored in the breakpoints
-  if(breakpoints.count(maybeBreakPointLocation)) {
-    Breakpoint& bp = breakpoints[maybeBreakPointLocation];
+  if(breakpoints.count(memory.getPC())) {
+    Breakpoint& bp = breakpoints[memory.getPC()];
     if(bp.isEnabled()) {
-      // Set PC back
-      memory.setPC(maybeBreakPointLocation);
       bp.disable();
-
       ptrace(PTRACE_SINGLESTEP, pid, nullptr, nullptr);
       waitForSignal();
       bp.enable();
     }
+  }
+}
+
+dwarf::die Debugger::getFunctionFromPC(uint64_t pc) {
+  for(auto &compilationUnit: pDwarf.compilation_units()) {
+    if(dwarf::die_pc_range(compilationUnit.root()).contains(pc)) {
+      for(const auto& die: compilationUnit.root()) {
+        if(die.tag == dwarf::DW_TAG::subprogram) {
+          return die;
+        }
+      }
+    }
+  }
+  spdlog::error("Cannot find function");
+  throw std::out_of_range{"Cannot find function"};
+}
+
+dwarf::line_table::iterator Debugger::getLineEntryFromPC(uint64_t pc) {
+  for(auto &compilationUnit: pDwarf.compilation_units()) {
+    if(dwarf::die_pc_range(compilationUnit.root()).contains(pc)) {
+      auto &lt = compilationUnit.get_line_table();
+      auto it = lt.find_address(pc);
+      if(it == lt.end()) {
+        spdlog::error("Cannot find line entry");
+        throw std::out_of_range{"Cannot find line entry"};
+      } else {
+        return it;
+      }
+    }
+  }
+  spdlog::error("Cannot find line entry");
+  throw std::out_of_range{"Cannot find line entry"};
+}
+
+void Debugger::initializeLoadAddress() {
+  if(pElf.get_hdr().type == elf::et::dyn) {
+    std::ifstream map("/proc/" + std::to_string(pid) + "/maps");
+    std::string address;
+    std::getline(map, address, '-');
+    map.close();
+    spdlog::info("The load address is {}", address);
+    loadAddress = std::stol(address, 0, 16);
+  }
+}
+
+uint64_t Debugger::offsetLoadAddress(uint64_t address) {
+  return address - loadAddress;
+}
+
+void Debugger::printSource(const std::string& fileName,unsigned line, unsigned nLinesContext) {
+  std::ifstream file {fileName};
+
+  // Work out a window around the desired value
+  auto startLine = line <= nLinesContext ? 1 : line - nLinesContext;
+  auto endLine = line + nLinesContext + (line < nLinesContext ?
+                 nLinesContext - line : 0) + 1;
+
+  char c{};
+  unsigned int currentLine = 1;
+  while(currentLine != startLine && file.get(c)) {
+    if(c == '\n') {
+      ++currentLine;
+    }
+  }
+
+  std::cout << (currentLine==line ? "> " : "  ");
+
+  while(currentLine <= endLine && file.get(c)) {
+    std::cout << c;
+    if(c == '\n') {
+      ++currentLine;
+      std::cout << (currentLine==line ? "> " : "  ");
+    }
+  }
+
+  std::cout << std::endl;
+  file.close();
+}
+
+siginfo_t Debugger::getSignalInfo() {
+  siginfo_t info;
+  ptrace(PTRACE_GETSIGINFO, pid, nullptr, &info);
+  return info;
+}
+
+void Debugger::handleSignaltrap(siginfo_t info) {
+  /*
+    * Handle `SIGTRAP`. It suffices to know that
+    * `SI_KERNEL` or `TRAP_BRKPT` will be sent when
+    * a breakpoint is hit, and `TRAP_TRACE` will be
+    * sent on single step completion.
+  */
+  switch(info.si_code) {
+  case SI_KERNEL:
+  case TRAP_BRKPT: {
+    // Put the PC back where it should be
+    memory.setPC(memory.getPC() - 1);
+    spdlog::info("Hit breakpoint at address 0x{:x}", memory.getPC());
+    uint64_t offsetPC = offsetLoadAddress(memory.getPC());
+    dwarf::line_table::iterator lineEntry = getLineEntryFromPC(offsetPC);
+    printSource(lineEntry->file->path, lineEntry->line);
+    return;
+  }
+  // This will be set if the signal was sent by single stepping
+  case TRAP_TRACE:
+    return;
+  default:
+    spdlog::info("Unknown SIGTRAP code {}", info.si_code);
+    return;
   }
 }
 
@@ -106,9 +218,22 @@ void Debugger::handleCommand(const std::string& line) {
 }
 
 void Debugger:: waitForSignal() {
-  int wait_status;
-  auto options = 0;
-  waitpid(pid, &wait_status, options);
+  int waitStatus;
+  int options = 0;
+  waitpid(pid, &waitStatus, options);
+
+  siginfo_t siginfo = getSignalInfo();
+
+  switch(siginfo.si_signo) {
+  case SIGTRAP:
+    handleSignaltrap(siginfo);
+    break;
+  case SIGSEGV:
+    spdlog::error("Yay, segfault. Reason: {}", siginfo.si_code);
+    break;
+  default:
+    spdlog::info("Got signal {}", strsignal(siginfo.si_signo));
+  }
 }
 
 void Debugger::run() {
@@ -119,6 +244,7 @@ void Debugger::run() {
     * is sent using the `waitpid` function
   */
   waitForSignal();
+  initializeLoadAddress();
 
   char* line = nullptr;
   // User linenoise library to handle user input for convenience
